35cc8860faa060bb52c591fe4a833d75bcb0f1ee
玉垒浮云 <yuleifuyunn@gmail.com>
2020-08-27 17:47:40 +0800

    Update Package

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
     ],
     targets: [
         .target(name: "PullToRefreshKit"),
-//        .testTarget(name: "PullToRefreshKitTests", dependencies: ["PullToRefreshKit"])
     ],
     swiftLanguageVersions: [
         .v5

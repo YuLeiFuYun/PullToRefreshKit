32f27acdc8cbd2f84c050542698d5003d1cff4bd
玉垒浮云 <yuleifuyunn@gmail.com>
2020-08-28 10:25:05 +0800

    更新 SPM 版本到 5.3

--- Package.swift
+++ Package.swift
@@ -1,16 +1,18 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "PullToRefreshKit",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "PullToRefreshKit", targets: ["PullToRefreshKit"]),
     ],
     targets: [
-        .target(name: "PullToRefreshKit"),
-    ],
-    swiftLanguageVersions: [
-        .v5
+        .target(
+            name: "PullToRefreshKit",
+            resources: [.process("Assets")]
+        )
     ]
 )
